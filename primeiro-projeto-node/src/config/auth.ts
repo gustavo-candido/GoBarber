71dfe35a6fcc11ffc8be36d857753a38566a133c
gustavo-candido <gustavocandidofreitas@gmail.com>
2020-05-09 05:46:23 -0300

    Revert "configure project for start front-web"
    This reverts commit 625c974f0f3a2212f00836f34ca8185efc4420cc.

---
+++ primeiro-projeto-node/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'c4bb5c1ecdaa2989b4a898fe7ebaa2a3',
+    expiresIn: '1d',
+  },
+};
--- web/src/App.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const App: React.FC = () => <h1>hello world</h1>;
-
-export default App;
