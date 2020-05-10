6339e8f262c5d541f6ed111fcd5ad2d28291b87e
gustavo-candido <gustavocandidofreitas@gmail.com>
2020-05-10 01:36:44 -0300

    {Estruturas das paginas} - Validação de login

--- web/src/components/Input/styles.ts
+++ web/src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import ToolTip from '../ToolTip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -20,6 +23,12 @@ export const Container = styled.div<ContainerProps>`
   & + div {
     margin-top: 8px;
   }
+
+  ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
 
   ${(props) =>
     props.isFocused &&
@@ -48,5 +57,22 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(ToolTip)`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    ::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
