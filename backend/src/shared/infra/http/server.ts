f3766a8f91e09b959d3a425b9a71b63faf9206c2
gustavo-candido <gustavocandidofreitas@gmail.com>
2020-05-17 07:01:38 -0300

    { Arquitetura e DDD } - Configurando Imports

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -2,13 +2,13 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
-
 import 'express-async-errors';
+
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
-import '../typeorm';
-import AppError from '../../errors/AppError';
-import uploadConfig from '../../../config/upload';
+import '@shared/infra/typeorm';
 
 const app = express();
 app.use(cors());
