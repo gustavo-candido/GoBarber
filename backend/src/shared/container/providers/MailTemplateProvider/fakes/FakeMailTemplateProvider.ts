558a2ca9f67ad21ed63428b4bb080a6844281708
gustavo-candido <gustavocandidofreitas@gmail.com>
2020-05-24 21:52:47 -0300

    { Perfil do usuário } - Template engine

--- backend/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ backend/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
 import IMailTemplateProvider from '../models/IMailTemplateProvider';
-import IMailTemplateProviderDTO from '../dtos/IParseMailTemplateDTO';
 
 class FakeTemplateProvider implements IMailTemplateProvider {
-  public async parse({ template }: IMailTemplateProviderDTO): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'mail content';
   }
 }
 
