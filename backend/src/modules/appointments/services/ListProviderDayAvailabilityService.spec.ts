6d1d4e1baaa7c7aee8a4093cbb4c68e63b4ef45d
gustavo-candido <gustavocandidofreitas@gmail.com>
2020-05-25 21:43:59 -0300

    { Agendamento } - Excluindo agendamentos antigos

--- backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -15,12 +15,16 @@ describe('ListProviderDayAvailability', () => {
   it('shout be able to list the the day avalability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 20, 8, 0, 0),
+      date: new Date(2020, 4, 20, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 20, 10, 0, 0),
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
     });
 
     const avalability = await listProviderDayAvailability.execute({
@@ -33,9 +37,12 @@ describe('ListProviderDayAvailability', () => {
     expect(avalability).toEqual(
       expect.arrayContaining([
         { hour: 8, avaliable: false },
-        { hour: 9, avaliable: true },
+        { hour: 9, avaliable: false },
         { hour: 10, avaliable: false },
-        { hour: 11, avaliable: true },
+        { hour: 13, avaliable: true },
+        { hour: 14, avaliable: false },
+        { hour: 15, avaliable: false },
+        { hour: 16, avaliable: true },
       ]),
     );
   });
