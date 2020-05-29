819a41880b654535d384dd3a07e9e9ca4755ab0e
gustavo-candido <gustavocandidofreitas@gmail.com>
2020-05-28 22:32:58 -0300

    { Personalizando para produção } - Emails pelo Amazon SES

---
+++ backend/src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'gobarber@candicode.com',
+      name: 'Candido GoBarber',
+    },
+  },
+} as IMailConfig;
